803eec00d186417351783129c94adee33db31e8e
Leandro Pintos <leopintos11@gmail.com>
2022-03-12 03:16:05 -0300

    Add Navigation to project

---
+++ app/src/main/java/com/lrp/hpcharacters/MainFragment.kt
@@ -0,0 +1,23 @@
+package com.lrp.hpcharacters
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.lrp.hpcharacters.databinding.FragmentMainBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding : FragmentMainBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+        binding = FragmentMainBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+}
