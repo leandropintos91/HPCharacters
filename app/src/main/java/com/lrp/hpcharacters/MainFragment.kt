1ff121cd0c6b42fe6789440294044189b87d7fd0
Leandro Pintos <leopintos11@gmail.com>
2022-03-13 23:43:39 -0300

    Add DrawerLayout to MainFragment

--- app/src/main/java/com/lrp/hpcharacters/MainFragment.kt
+++ app/src/main/java/com/lrp/hpcharacters/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.lrp.hpcharacters.databinding.FragmentMainBinding
 
@@ -19,5 +20,12 @@ class MainFragment : Fragment() {
         super.onCreateView(inflater, container, savedInstanceState)
         binding = FragmentMainBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.toolbar.setNavigationOnClickListener {
+            binding.drawerLayout.open()
+        }
     }
 }
